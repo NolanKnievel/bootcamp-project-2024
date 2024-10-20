5014ac8b996619b069796ad14db0f168e4e2752b
Nolan Knievel <nolan.knievel@icloud.com>
2024-10-19 22:35:31 -0700

    fix: remove stray text from layout.tsx

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/> // This sets the navbar on every page 
+        <Navbar/> 
         {children}
       </body>
     </html>
