a1ff321f7c1488c9cb8df7ab2c713b30b561ce10
Nolan Knievel <nolan.knievel@icloud.com>
2024-10-19 22:39:17 -0700

    fix: change a tags to Link

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -6,19 +6,31 @@ export default function Navbar() {
   return (
     // replace everything in between the <header> & <header /> tags
     // with your navbar code from your earlier milestones
-    // NOTE: we use "class" in HTML but React is quirky so we have to 
+    // NOTE: we use "class" in HTML but React is quirky so we have to
     // change all the "class" to "className"
-    <header className={style.navbar} >
-        <nav className="navbar">
-        <h1 className="logo"><a href="index.html">&ltNolanKnievel&gt</a></h1>
+    <header className={style.navbar}>
+      <nav className="navbar">
+        <h1 className="logo">
+          <a href="index.html">&ltNolanKnievel&gt</a>
+        </h1>
         <ul className="nav-list">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="resume.html">Resume</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="blog.html">Blog</a></li>
+          <li>
+            <Link href="index.html">Home</Link>
+          </li>
+          <li>
+            <Link href="about.html">About</Link>
+          </li>
+          <li>
+            <Link href="resume.html">Resume</Link>
+          </li>
+          <li>
+            <Link href="contact.html">Contact</Link>
+          </li>
+          <li>
+            <Link href="blog.html">Blog</Link>
+          </li>
         </ul>
-        </nav>
+      </nav>
     </header>
   );
 }
