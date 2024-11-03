feda6ee84d3124d8219e8299a0e93d462fc24092
Nolan Knievel <nolan.knievel@icloud.com>
2024-11-03 15:01:21 -0800

    feat: add footer module and css

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+    <p>© 2023 Nolan's Website | All Rights Reserved</p>
+    <br/>
+    <img src="./meMidairTransparent.png" height="50px"/>
+  </footer>
+
+  );
+}
