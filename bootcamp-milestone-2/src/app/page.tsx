4e094c2603848f2e0048f8f2b6ae571db3adaaac
Nolan Knievel <nolan.knievel@icloud.com>
2024-11-06 18:03:25 -0800

    feat: properly display blog posts

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,18 +1,34 @@
+import blogs from "./blogData";
+import BlogPreview from "@/components/blogPreview";
+
 export default function Home() {
   return (
     <div>
-    <div className="home-border-wrap">
-      <main className="home-main">
-        <h1 className="page-title">Nolan Knievel</h1>
-        <h1 className="SLO">San Luis Obispo, CA</h1>
+      <div className="home-border-wrap">
+        <main className="home-main">
+          <h1 className="page-title">Nolan Knievel</h1>
+          <h1 className="SLO">San Luis Obispo, CA</h1>
 
-        <div className="about">
-          <div className="about-image">
-            <img className = "home-img" src="./images/meSteezTransparent.png" width="700" />
+          <div className="about">
+            <div className="about-image">
+              <img
+                className="home-img"
+                src="./images/meSteezTransparent.png"
+                width="700"
+              />
+            </div>
+
+            <div id="blog-container">
+              {blogs.map(
+                (blog) => (
+                  <BlogPreview {...blog}/>
+                ) 
+                
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
     </div>
   );
 }
