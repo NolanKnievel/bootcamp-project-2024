7fddd84af9b07dc8f14e2cdb162cd82d9b607dae
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-01 20:56:09 -0800

    feat: incorporate portfolio entries into portfolio page

--- bootcamp-milestone-3/src/app/portfolio/page.tsx
+++ bootcamp-milestone-3/src/app/portfolio/page.tsx
@@ -1,8 +1,37 @@
-export default function Home() {
+import PortfolioEntryPreveiw from "@/components/portfolioEntryPreview";
+import { connectPortfolioDB } from "@/database/database";
+import PortfolioEntry from "@/database/portfolioEntrySchema";
+
+async function getPortfolioEntries() {
+  const url: string = process.env.PORTFOLIO_URI as string;
+
+  await connectPortfolioDB();
+  try {
+    const entries = await PortfolioEntry.find().sort({ date: -1 }).orFail();
+
+    const formattedEntries = entries.map((entry) => ({
+      title: entry.title,
+      description: entry.description,
+      date: entry.date.toString(),
+    }));
+
+    return formattedEntries;
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function Home() {
+  const entries = await getPortfolioEntries();
   return (
     <div>
       <div className="border-wrap">
         <h1 className="page-title">Portfolio</h1>
+        <div id="portfolio-container">
+          {entries.map((entry) => (
+            <PortfolioEntryPreveiw {...entry} />
+          ))}
+        </div>
       </div>
     </div>
   );
