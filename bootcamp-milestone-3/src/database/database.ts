ffae40a720c4eb1e4151de21fffcd2098d221bdc
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-01 20:14:54 -0800

    feat: change connectDB into two functions for separate portfolio and blog connections

--- bootcamp-milestone-3/src/database/database.ts
+++ bootcamp-milestone-3/src/database/database.ts
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const url: string = process.env.MONGO_URI as string;
-let connection: typeof mongoose;
+const blog_url: string = process.env.MONGO_URI as string;
+let blogConnection: typeof mongoose;
 
-/**
- * Makes a connection to a MongoDB database. If a connection already exists, does nothing
- * Call this function at the start of api routes and data fetches
- * @returns {Promise<typeof mongoose>}
- */
-const connectDB = async () => {
-  if (!connection) {
-    connection = await mongoose.connect(url);
-    return connection;
+export const connectBlogDB = async () => {
+  if (!blogConnection) {
+    blogConnection = await mongoose.connect(blog_url);
+    return blogConnection;
   }
 };
 
-export default connectDB;
+const portfolio_url: string = process.env.MONGO_URI as string;
+let portfolioConnection: typeof mongoose;
+
+export const connectPortfolioDB = async () => {
+  if (!portfolioConnection) {
+    portfolioConnection = await mongoose.connect(portfolio_url);
+    return portfolioConnection;
+  }
+};
 
