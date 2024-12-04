7f5ad34c2eace117d8c2189e8c3d38472f9ea78e
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-03 21:48:31 -0800

    fix: clear out blog page

--- bootcamp-milestone-3/src/app/blog/page.tsx
+++ bootcamp-milestone-3/src/app/blog/page.tsx
@@ -2,43 +2,14 @@ import BlogPreview from "@/components/blogPreview";
 import connectDB from "@/database/database";
 import Blog from "@/database/blogSchema";
 
-async function getBlogs() {
-  const url: string = process.env.MONGO_URI as string;
-
-  await connectBlogDB();
-  try {
-    const blogs = await Blog.find().sort({ date: -1 }).orFail();
-
-    const formattedBlogs = blogs.map((blog) => ({
-      title: blog.title,
-      description: blog.description,
-      date: blog.date.toString(),
-      slug: blog.slug,
-    }));
-
-    return formattedBlogs;
-  } catch (err) {
-    return null;
-  }
-}
 
 export default function Home() {
-  const blogs = await getBlogs();
 
   return (
     <div>
       <div className="border-wrap">
         <h1 className="page-title">Blog</h1>
 
-        <div className="about">
-          <div id="blog-container">
-            {blogs != null ? (
-              blogs.map((blog) => <BlogPreview {...blog} />)
-            ) : (
-              <p>No blogs available.</p>
-            )}
-          </div>
-        </div>
       </div>
     </div>
   );
