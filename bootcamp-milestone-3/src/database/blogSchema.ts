c4818a6a8f881e22f2620942238cf2dc34c2cf7a
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-14 15:18:57 -0800

    feat: add comments field to blog Schema

--- bootcamp-milestone-3/src/database/blogSchema.ts
+++ bootcamp-milestone-3/src/database/blogSchema.ts
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import commentSchema from "./commentSchema";
+import { IComment } from "@/database/commentSchema";
 
 type Blog = {
   title: string;
@@ -15,9 +17,9 @@ const blogSchema = new Schema<Blog>({
   date: { type: Date, required: false, default: new Date() },
   description: { type: String, required: true },
   content: { type: String, required: true },
+  comments: { type: [commentSchema], required: false, default: [] },
 });
 
-const Blog = mongoose.models['blogs'] ||
-    mongoose.model('blogs', blogSchema);
+const Blog = mongoose.models["blogs"] || mongoose.model("blogs", blogSchema);
 
 export default Blog;
