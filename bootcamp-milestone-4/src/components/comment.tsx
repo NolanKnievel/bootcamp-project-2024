9f80affda9fbc0ff0d090cbad3385dc9a49cac40
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-21 12:27:06 -0800

    fix: correct time to string converstion

--- bootcamp-milestone-4/src/components/comment.tsx
+++ bootcamp-milestone-4/src/components/comment.tsx
@@ -1,16 +1,17 @@
 import { IComment } from "@/database/commentSchema";
+import style from "./comment.module.css";
 
 type CommentProps = {
   comment: IComment;
 };
 
 function parseCommentTime(time: Date) {
-  return time.toDateString;
+  return time.toString();
 }
 
 function CommentComponent({ comment }: CommentProps) {
   return (
-    <div>
+    <div className={style.comment}>
       <h4>{comment.user}</h4>
       <p>{comment.comment}</p>
       <span>{parseCommentTime(comment.time)}</span>
