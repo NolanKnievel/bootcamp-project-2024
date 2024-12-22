5589abbb1a84ac6d76667a23cd7e669605399c0a
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-22 12:23:42 -0800

    fix: type fixes and function restructuring

--- bootcamp-milestone-4/src/app/api/contact/route.ts
+++ bootcamp-milestone-4/src/app/api/contact/route.ts
@@ -1,9 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 import emailjs from "emailjs-com";
 
+async function sendEmail(name: string, email: string, message: string) {
+  const EMAIL_JS_SERVICE_ID = "service_p8j5x0j";
+  const EMAIL_JS_TEMPLATE_ID = "template_b7682mh";
+
+  const templateParams = {
+    to_name: "Nolan",
+    from_name: name,
+    message: message,
+    reply_to: email,
+  };
+
+  await emailjs
+    .send(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, templateParams)
+    .then(
+      (response) => {
+        console.log("Email sent successfully:", response);
+      },
+      (error) => {
+        console.error("Failed to send email:", error);
+      }
+    );
+}
+
 export async function POST(req: NextRequest) {
   try {
-
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
@@ -13,33 +35,11 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const EMAIL_JS_SERVICE_ID = "service_p8j5x0j";
-    const EMAIL_JS_TEMPLATE_ID = "template_b7682mh";
     const EMAIL_JS_PUBLIC_KEY = "UdF-oso8jxuFTppKF";
 
-    emailjs.init({ publicKey: EMAIL_JS_PUBLIC_KEY });
+    emailjs.init(EMAIL_JS_PUBLIC_KEY);
 
-    async function sendEmail() {
-      const templateParams = {
-        to_name: "Nolan",
-        from_name: name,
-        message: message,
-        reply_to: email,
-      };
-
-        await emailjs
-        .send(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, templateParams)
-        .then(
-          (response) => {
-            console.log("Email sent successfully:", response);
-          },
-          (error) => {
-            console.error("Failed to send email:", error);
-          }
-        );
-    }
-
-    sendEmail();
+    sendEmail(name, email, message);
 
     return NextResponse.json(
       { message: "Message sent successfully!" },
