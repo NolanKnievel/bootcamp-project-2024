96bb9fcbc8432f5c0675d28a2c44b86e9e9a75b7
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-22 12:36:42 -0800

    fix: resolve date to string

--- bootcamp-milestone-4/src/components/blogPreview.tsx
+++ bootcamp-milestone-4/src/components/blogPreview.tsx
@@ -5,14 +5,12 @@ import Link from "next/link";
 
 export default function BlogPreview(props: Blog) {
   return (
-    // include image in divs
     <Link href={`/blog/${props.slug}`}>
       <div className={style.post}>
         <h3> {props.title} </h3>
         <div>
           <p>{props.description}</p>
-          <p>{props.date}</p>
-          <img src={props.image} />
+          <p>{props.date.toDateString()}</p>
         </div>
       </div>
     </Link>
