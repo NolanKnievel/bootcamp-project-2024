0c9f2e539d730b69457395b80cd7ebda56a1ad41
Nolan Knievel <nolan.knievel@icloud.com>
2024-12-22 11:59:39 -0800

    fix: add schema import

--- bootcamp-milestone-4/src/database/commentSchema.ts
+++ bootcamp-milestone-4/src/database/commentSchema.ts
@@ -1,15 +1,15 @@
+import { Schema } from "mongoose";
 
 export type IComment = {
-		user: string;
-		comment: string;
-		time: Date;
-}
+  user: string;
+  comment: string;
+  time: Date;
+};
 
 const commentSchema = new Schema<Comment>({
-    user: {type: String, required: true},
-    comment: {type: String, required: true},
-    time: {type: Date, required: true},
-  });
-  
+  user: { type: String, required: true },
+  comment: { type: String, required: true },
+  time: { type: Date, required: true },
+});
 
 export default commentSchema;
